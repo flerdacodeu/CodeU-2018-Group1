0da594f99c55811a61ce2a5850288b0afdaa156b
vltt <32797726+vltt@users.noreply.github.com>
2018-05-21 14:32:59 +0300

    Update Q1.cpp

--- vltt/assignment1/Q1.cpp
+++ vltt/assignment1/Q1.cpp
@@ -6,52 +6,53 @@
 #include <unordered_set>
 #include <unordered_map>
 
-//is_case_insensitive == 0 -  sensitive, is_case_insensitive == 1 - insensitive
+//is_case_sensitive == 1 -  sensitive, is_case_sensitive == 0 - insensitive
 
-void split_by_word(std::string &s, std::unordered_map<std::string, int> &m, bool is_case_insensitive) {
+
+void split_by_word(std::string &sentence, std::unordered_map<std::string, int> &word_counts, bool is_case_sensitive) {
     std::string cur;
-    for (int i = 0; i <= s.length(); i++) {
-        if (i == s.length() || isspace(s[i])) {
+    for (int i = 0; i <= sentence.length(); i++) {
+        if (i == sentence.length() || !isalpha(sentence[i]) && !isdigit(sentence[i])) {
             if (!cur.empty()) {
                 sort(cur.begin(), cur.end());
-                m[cur]++;
+                word_counts[cur]++;
                 cur = "";
             }
-            if (i == s.length()) return;
-        } 
-        else if (is_case_insensitive) cur += tolower(s[i]);
-        else cur += s[i];
+            if (i == sentence.length()) return;
+        }
+        else if (is_case_sensitive) cur += sentence[i];
+        else cur += tolower(sentence[i]);
     }
 }
 
-bool is_anagram(std::string &a, std::string &b, bool ch) {
-    std::unordered_map<std::string, int> m_b;
+bool is_anagram(std::string &sentence_a, std::string &sentence_b, bool is_case_sensitive) {
+    std::unordered_map<std::string, int> word_counts_b;
     std::string cur;
-    split_by_word(b, m_b, is_case_insensitive);
-    for (int i = 0; i <= a.length(); i++) {
-        if (i == a.length() || isspace(a[i])) {
+    split_by_word(sentence_b, word_counts_b, is_case_sensitive);
+    for (int i = 0; i <= sentence_a.length(); i++) {
+        if (i == sentence_a.length() || !isalpha(sentence_a[i]) && !isdigit(sentence_a[i])) {
             if (!cur.empty()) {
                 sort(cur.begin(), cur.end());
-                if (m_b.find(cur) != m_b.end()) {
-                    if (--m_b[cur] == 0) m_b.erase(cur);
+                if (word_counts_b.find(cur) != word_counts_b.end()) {
+                    if (--word_counts_b[cur] == 0) word_counts_b.erase(cur);
                     cur = "";
                 }
                 else return false;
             }
-            if (i == a.length()) break;
+            if (i == sentence_a.length()) break;
         }
-        else if (is_case_insensitive) cur += tolower(a[i]);
-        else cur += a[i];
+        else if (is_case_sensitive) cur += sentence_a[i];
+        else cur += tolower(sentence_a[i]);
     }
-    return m_b.empty();
+    return word_counts_b.empty();
 }
 
 int main() {
-    std::string a, b;
-    getline(std::cin, a);
-    getline(srd::cin, b);
-    bool ch;
-    std::cin >> ch;
-    std::cout << f(a, b, ch) << std::endl;
+    std::string sentence_a, sentence_b;
+    getline(std::cin, sentence_a);
+    getline(std::cin, sentence_b);
+    bool is_case_sensitive;
+    std::cin >> is_case_sensitive;
+    std::cout << is_anagram(sentence_a, sentence_b, is_case_sensitive) << std::endl;
     return 0;
 }
