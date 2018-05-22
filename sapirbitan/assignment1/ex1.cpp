1e33f0f8b8473526d9503aac3371d3a378b989e1
sapirbitan <32384529+sapirbitan@users.noreply.github.com>
2018-05-22 21:02:02 +0300

    Update ex1.cpp

--- sapirbitan/assignment1/ex1.cpp
+++ sapirbitan/assignment1/ex1.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 // case sensitive
-int isAnagram(const string word1, string word2, bool sensitive) {
+int isAnagram(const string word1, string word2) {
 	if (word1.size() != word2.size())//checks if the words could be anagrams by the size of them
 		return 0;
 	int size1 = word1.size();
@@ -25,4 +25,28 @@ int isAnagram(const string word1, string word2, bool sensitive) {
 		//if the temp string is empty and word2 is empty - it means we have "used" all the words 
 		//which means the words are anagrams
 	}
+}
+int main() {
+	//test1
+	bool test1 = isAnagram("Silent", "listen");
+	cout << test1;
+	//test2
+	bool test2 = isAnagram("what", "thaw");
+	cout << test2;
+	//test3
+	bool test3 = isAnagram("car", "arc");
+	cout << test3;
+	//test4
+	bool test4 = isAnagram(" ", "");
+	cout << test4;
+	//test5
+	bool test5 = isAnagram("dusty", "studySS");
+	cout << test5;
+	//test5
+	bool test6 = isAnagram("BanaNa", "BaNana");
+	cout << test6;
+	//test7
+	bool test7 = isAnagram("", "");
+	cout << test7;
+	cin >> test1;
 }
