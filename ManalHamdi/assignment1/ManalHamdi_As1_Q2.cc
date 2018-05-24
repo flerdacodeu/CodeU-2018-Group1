d148a00e13188406792223ea424ce01fdf68ae1b
Manal Hamdi <manal.hamdi@yahoo.fr>
2018-05-24 16:09:03 +0000

    Update ManalHamdi_As1_Q2.cc
    Thanks for your review! I modified the code according to your comments.

--- ManalHamdi/assignment1/ManalHamdi_As1_Q2.cc
+++ ManalHamdi/assignment1/ManalHamdi_As1_Q2.cc
@@ -1,9 +1,10 @@
 #include <iostream>
-using namespace std;
+
 struct node {
     int data;
     node *next;
 };
+
 class list {
     private:
     node *head, *tail;
@@ -11,17 +12,16 @@ class list {
     public:
     int listSize = 0;
     list() {
-      head = NULL;
-      tail = NULL;
+      head = nullptr;
+      tail = nullptr;
     }
-    void createnode(int value) {
+    void appendToList(int value) {
       node *temp = new node;
       temp->data = value;
-      temp->next = NULL;
-      if(head == NULL) {
+      temp->next = nullptr;
+      if(head == nullptr) {
         head = temp;
         tail = temp;
-        temp = NULL;
       }
       else {	
         tail->next = temp;
@@ -30,25 +30,22 @@ class list {
       listSize++;
     }
     int displayKthToLast(int k) {
-	    node *temp = new node;
-	    temp = head;
+	    node *temp = head;
 	    for (int i = 0; i < listSize - k - 1; i++) {
 	    	temp = temp -> next;
 	    }
-	    
 	    return temp->data ;
-	    
-    	
   	} 
-};	
+};
+
 int main (void) {
 	list l;
 	for (int i = 0; i < 4; i++)
 		l.createnode(i);
-	int k; cin >> k;
-	if (k >= l.listSize)
-		cout << "Not possible" << endl;
+	int k; std::cin >> k;
+	if (k >= l.listSize || k < 0)
+		std::cout << "Not possible" << std::endl;
 	else
-		cout << l.displayKthToLast(k) << endl;
+		std::cout << l.displayKthToLast(k) << std::endl;
 	return 0;
 }
