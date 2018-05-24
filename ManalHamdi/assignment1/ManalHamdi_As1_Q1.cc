302d73df7cb1c0f96ecc490239ceb7f373e9bfc7
Manal Hamdi <manal.hamdi@yahoo.fr>
2018-05-24 17:17:56 +0000

    Update ManalHamdi_As1_Q1.cc

--- ManalHamdi/assignment1/ManalHamdi_As1_Q1.cc
+++ ManalHamdi/assignment1/ManalHamdi_As1_Q1.cc
@@ -2,59 +2,54 @@
 #include <vector>
 #include <algorithm>
 #include <ctype.h>
-using namespace std;
 
-bool isAnagramCaseSensitive(string s1, string s2) {
+bool IsAnagram(std::string s1, std::string s2, bool is_case_sensitive) {
+	std::vector<char> s1_chars;
+	std::vector<char> s2_chars;
+
 	if (s1.size() != s2.size())
 		return false;
 
-	sort(s1.begin(), s1.end());
-	sort(s2.begin(), s2.end());
+	if (is_case_sensitive) {
+		s1_chars.push_back(s1[i]);
+		s2_chars.push_back(s2[i]);
+	}
+	else {
+		s1_chars.push_back(tolower(s1[i]));
+		s2_chars.push_back(tolower(s2[i]));
+	}
 
-	for (int i = 0; i < s1.size(); i++) {
-		if (s1[i] != s2[i])
-			return false;
-	}
-	return true;
+	std::sort(s1_chars.begin(), s1_chars.end());
+	std::sort(s2_chars.begin(), s2_chars.end());
+
+	return s1_chars == s2_chars;
 }
-bool isAnagramCaseInsensitive(string s1, string s2) {
-	if (s1.size() != s2.size())
-		return false;
-	for (int i = 0; i < s1.size(); i++) {
-		s1[i] = tolower(s1[i]);
-		s2[i] = tolower(s2[i]);
-	}
-	return isAnagramCaseSensitive(s1, s2);
-}
-bool isSentenceAnagram (string sent1, string sent2) {
-	vector <string> vSent1, vSent2;
-	if (sent1.size() != sent2.size())
-		return false;
+std::vector<std::string> SplitByWords(const std::string& sentence) {
 	int index_start = 0, index_end = 0;
-	for (int i = 0; i < sent1.size(); i++) {
-		if (sent1[i] == ' ' || i == sent1.size() - 1) {
+	vector<std::string> sentence_words;
+	for (int i = 0; i < sentence.size(); i++) {
+		if (sentence[i] == ' ' || i == sentence.size() - 1) {
 			index_end = i;
-			string str = sent1.substr(index_start, index_end - index_start + 1);
-			sort(str.begin(), str.end());
-			vSent1.push_back(str);
+			string str = sentence.substr(index_start, index_end - index_start + 1);
+			std::sort(str.begin(), str.end());
+			sentence_words.push_back(str);
 			index_start = index_end + 1;
 		}
 	}
-	for (int i = 0; i < sent2.size(); i++) {
-		if (sent2[i] == ' ' || i == sent2.size() - 1) {
-			index_end = i;
-			string str = sent2.substr(index_start, index_end - index_start + 1);
-			sort(str.begin(), str.end());
-			vSent2.push_back(str);
-			index_start = index_end + 1;
-		}
-	}
-
-	sort(vSent1.begin(), vSent1.end());
-	sort(vSent2.begin(), vSent2.end());
-	for (int i = 0; i < vSent1.size(); i++) 
-		if (!isAnagramCaseSensitive(vSent1[i],vSent2[i]))
+	return sentence_words;
+}
+bool IsSentenceAnagram (std::string sent1, std::string sent2, bool is_case_sensitive) {
+	std::vector <std::string> sent1_words = splitByWords(sent1);
+	std::vector <std::string> sent2_words = splitByWords(sent2);
+	
+	if (sent1_words.size() != sent1_words.size())
+		return false;
+	
+	std::sort(sent1_words.begin(), sent1_words.end());
+	std::sort(sent2_words.begin(), sent2_words.end());
+	
+	for (int i = 0; i < sent1_words.size(); i++) 
+		if (!IsAnagram(sent1_words[i], sent2_words[i], is_case_sensitive))
 			return false;
 	return true;
-
 }
