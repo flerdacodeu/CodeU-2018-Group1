f182051baef89ac46631d3053273b67d90e960f1
sapirbitan <32384529+sapirbitan@users.noreply.github.com>
2018-05-24 09:30:13 +0300

    Update ex2.cpp

--- sapirbitan/assignment1/ex2.cpp
+++ sapirbitan/assignment1/ex2.cpp
@@ -2,45 +2,52 @@
 #include <string>
 using namespace std;
 
-struct some_struct {
+struct Node {
 	int num;
-	some_struct* s=NULL;
+	Node* next_node=NULL;
 };
 /* I am assuming each element in the linked list contains a pointer to the next element in the list,
 also, I am assuming my function gets the "head" of the linked list and 'k'	*/
 
-some_struct find_kth_element(some_struct first_element, int k) {
+Node find_kth_element(Node& head, int k) {
 	int num_of_elements = 1;
-	some_struct* current = &first_element;
-	//if k>num_of_elements the function will return the "head" of the list
-	some_struct kth=first_element;// the kth last element
-	//finds out hoe many elements are on the linked list
-	while (current->s!= NULL) {
+	Node* current = &head;
+	//finds out how many elements are on the linked list
+	while (current->next_node != NULL) {
 		num_of_elements += 1;
-		current = current->s;
+		current = current->next_node;
 	}
+	//if k>num_of_elements the function will return the "head" of the list
+	Node kth = head;// the kth last element
 	//"going" to the kth last element
 	for (int i = 1; i < num_of_elements-k;i++) {
-		kth = *kth.s;
+		kth = *kth.next_node;
 	}
 	return kth;
 }
+
 int main() {
-	//test
-	some_struct a;
+	Node a;
 	a.num = 21;
-	some_struct b;
-	a.s = &b;
+	Node b;
+	a.next_node = &b;
 	b.num = 20;
-	some_struct c;
-	b.s = &c;
+	Node c;
+	b.next_node = &c;
 	c.num = 19;
-	some_struct d;
-	c.s = &d;
+	Node d;
+	c.next_node = &d;
 	d.num = 18;
-	some_struct e;
-	d.s = &e;
+	Node e;
+	d.next_node = &e;
 	e.num = 17;
-	some_struct kth = find_kth_element(a, 6);
-	cout << kth.num;
+	//test1
+	Node kth = find_kth_element(a, 6);
+	cout << kth.num << endl;
+	//test2
+	kth = find_kth_element(a, 5);
+	cout << kth.num << endl;
+	//test3
+	kth = find_kth_element(c, 2);
+	cout << kth.num << endl;
 }
