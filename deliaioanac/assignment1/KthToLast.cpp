8712c3a5e9af33635437360cfb812a1dbb9981d6
deliaioanac <delia.ioanaa@gmail.com>
2018-06-03 18:12:43 +0300

    Add files via upload

--- deliaioanac/assignment1/KthToLast.cpp
+++ deliaioanac/assignment1/KthToLast.cpp
@@ -1,46 +1,80 @@
 #include <iostream>
 #include <stdlib.h>
-
-using namespace std;
+#include <vector>
 
 template <class T>
 struct ListNode {
     T data;
-    struct ListNode *next;
+    ListNode *next;
+    ListNode(T x) : data(x), next(nullptr) {} 
 };
 
-template <typename T>
-T getKthToLast(ListNode<T> *head, int k) {
-    // Sanity check
-    if (k < 0) {
-        cerr << "getKthToLast: k must be a positive number!" << endl;
-        exit(EXIT_FAILURE);
-    }
+template <class T>
+class CustomList {
+    private:
+        ListNode<T> *head = nullptr;
+    public:
+        // Adds the element to the list
+        void addElement(T data) {
+            ListNode<T> *new_head = new ListNode<T>(data);
+            
+            if (!new_head) {
+                std::cerr << "new failed" << std::endl;
+                return;
 
-    ListNode<T> *follower = head;
+            }
 
-    // Move the head up by k positions
-    // (the follower will be left behind by k positions so that,
-    //  when the head reached the end of the list, the follower is
-    //  on the kth to last element)
-    while (head != NULL && k > 0) {
-        head = head->next;
-        k --;
-    }
+            new_head->next = head;
+            head = new_head;
+        }
 
-    // If the head is NULL, it means there were less than k elements
-    // in the list, so we can not find the kth to last element
-    if (head == NULL) {
-        cerr << "getKthToLast: Not enough elements!" << endl;
-        exit(EXIT_FAILURE);
-    }
+        // Adds all the elements to the list
+        void addElements(const std::vector<T> &elements) {
+            for (T elem : elements) {
+                addElement(elem);
+            }
+        }
 
-    // Move both pointers until the head reached the end of the list and,
-    // as explained, the follower will be on the kth to last element
-    while (head != NULL && head->next != NULL) {
-        head = head->next;
-        follower = follower->next;
-    }
+        // Destroys the entire list
+        void destroyList() {
+            while (head) {
+                ListNode<T> *aux = head->next;
+                delete head;
+                head = aux;
+            }
+        }
 
-    return follower->data;
-}
+        T getKthToLast(int k) {
+            // Sanity check
+            if (k < 0) {
+                throw std::out_of_range("index must be a positive number");
+            }
+
+            ListNode<T> *follower = head;
+            ListNode<T> *leader = head;
+
+            // Move the head up by k positions
+            // (the follower will be left behind by k positions so that,
+            //  when the head reached the end of the list, the follower is
+            //  on the kth to last element)
+            while (leader != nullptr && k > 0) {
+                leader = leader->next;
+                k --;
+            }
+
+            // If the head is NULL, it means there were less than k elements
+            // in the list, so we can not find the kth to last element
+            if (leader == nullptr) {
+                throw std::out_of_range("index is too high");
+            }
+
+            // Move both pointers until the head reached the end of the list and,
+            // as explained, the follower will be on the kth to last element
+            while (leader != nullptr && leader->next != nullptr) {
+                leader = leader->next;
+                follower = follower->next;
+            }
+
+            return follower->data;
+        }
+};
--- deliaioanac/assignment1/KthToLastUnitTests.cpp
+++ deliaioanac/assignment1/KthToLastUnitTests.cpp
@@ -4,66 +4,36 @@
 namespace {
     class KthToLastTest : public ::testing::Test {
         protected:
-            ListNode<int> *head;
-
-            virtual void addNext(ListNode<int> *copy, int data) {
-                copy->next = (ListNode<int> *) malloc(sizeof(ListNode<int>));
-                if (!copy->next) {
-                    cerr << "malloc failed" << endl;
-                    exit(EXIT_FAILURE);
-
-                }
-                copy->next->data = data;
-            }
+            CustomList<int> list;
 
             virtual void SetUp() {
-                head = (ListNode<int> *) malloc(sizeof(ListNode<int>));
-                if (!head) {
-                    cerr << "malloc failed" << endl;
-                    exit(EXIT_FAILURE);
-                }
-
-                head->data = 0;
-                ListNode<int> *copy = head; 
-                addNext(copy, 1);
-                copy = copy->next;
-                addNext(copy, 2);
-                copy = copy->next;
-                addNext(copy, 3);
-                copy = copy->next;
-                addNext(copy, 4);
-                copy = copy->next;
-                copy->next = NULL;
+                std::vector<int> elements = {4, 3, 2, 1};
+                list.addElements(elements);
             }
 
             virtual void TearDown() {
-                ListNode<int> *copy;
-                while (head != NULL) {
-                    copy = head->next;
-                    free(head);
-                    head = copy;
-                }
+                list.destroyList(); 
             }
     };
 
     TEST_F(KthToLastTest, GetFirst) {
-        ASSERT_EQ(4, getKthToLast(head, 0));
+        ASSERT_EQ(4, list.getKthToLast(0));
     }
 
     TEST_F(KthToLastTest, GetMiddle) {
-        ASSERT_EQ(2, getKthToLast(head, 2));
+        ASSERT_EQ(2, list.getKthToLast(2));
     }
 
     TEST_F(KthToLastTest, GetLast) {
-        ASSERT_EQ(0, getKthToLast(head, 4));
+        ASSERT_EQ(1, list.getKthToLast(3));
     }
 
     TEST_F(KthToLastTest, OutOfBoundsUpper) {
-        EXPECT_EXIT(getKthToLast(head, 10), ::testing::ExitedWithCode(EXIT_FAILURE), "");
+        EXPECT_THROW(list.getKthToLast(10), std::out_of_range);
     }
 
     TEST_F(KthToLastTest, OutOfBoundsLower) {
-        EXPECT_EXIT(getKthToLast(head, -1), ::testing::ExitedWithCode(EXIT_FAILURE), "");
+        EXPECT_THROW(list.getKthToLast(-1), std::out_of_range);
     }
 }
 
