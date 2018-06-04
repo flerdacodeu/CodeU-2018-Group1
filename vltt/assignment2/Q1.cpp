3be1355e00eac7b53c4b242f1f7839ebc9035b67
vltt <32797726+vltt@users.noreply.github.com>
2018-06-04 17:57:01 +0300

    Update Q1.cpp

--- vltt/assignment2/Q1.cpp
+++ vltt/assignment2/Q1.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <algorithm>
 #include <iomanip>
+#include <vector>
 
 struct node {
     node *left = nullptr;
@@ -8,16 +9,20 @@ struct node {
     int value;
 };
 
-bool dfs(const int key, const node *cur) {
+// dfs implements the depth-first search algorithm, put all of the ancestors of the given key in the vector.
+// dfs assumes that the given tree only contains unique nodes.
+bool dfs(const int key, const node *cur, std::vector<const node*> &vector_of_ancestors) {
     if (cur == nullptr) return false;
     if (cur->value == key) {
         return true;
     }
-    bool is_found = dfs(key, cur->left) || dfs(key, cur->right);
-    if (is_found) std::cout << cur->value << ' ';
+    bool is_found = dfs(key, cur->left, vector_of_ancestors) || dfs(key, cur->right, vector_of_ancestors);
+    if (is_found) vector_of_ancestors.push_back(cur);
     return is_found;
 }
-void ancestors_of_key(const int key, const node *root) {
-    dfs(key, root);
-    std::cout << std::endl;
+std::vector<const node*>  ancestors_of_key(const int key, const node *root) {
+    std::vector<const node*> vector_of_ancestors;
+    dfs(key, root, vector_of_ancestors);
+    reverse(vector_of_ancestors.begin(), vector_of_ancestors.end());
+    return vector_of_ancestors;
 }
