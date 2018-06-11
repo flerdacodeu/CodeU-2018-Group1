7d3e688b698142aaa62f41e2bb4f2f0c2b6570ba
Manal Hamdi <manal.hamdi@yahoo.fr>
2018-06-11 17:06:12 +0200

    Update Assignments2_Q1.cc
    Part1 of commits (not complete yet)

--- ManalHamdi/assignment2/Assignments2_Q1.cc
+++ ManalHamdi/assignment2/Assignments2_Q1.cc
@@ -7,14 +7,18 @@ struct Node {
 	Node* parent;
 	Node* left_child;
 	Node* right_child; 
+	~Node() {
+		delete right_child;
+        	delete left_child;
+	}
 };
 class Tree {
 public:
 	Node* root;
-	Tree() {
-		root = nullptr;
-	}
-	Node* CreateNode(int data) {
+	~Tree(){
+           delete root;
+        }
+	static Node* CreateNode(int data) {
 		Node* temp = new Node;
 		temp->data = data;
 		temp->left_child = nullptr;
@@ -22,13 +26,14 @@ public:
 		temp->parent = nullptr;
 		return temp;
 	} 
-	void AppendTree(Node* parent, Node* node_to_append, bool append_to_left) {
+	static void AppendTree(Node* parent, Node* node_to_append, bool append_to_left) {
 		node_to_append->parent = parent;
 		if (append_to_left) {
 			parent->left_child = node_to_append;
 		}
-		else
+		else {
 			parent->right_child = node_to_append;
+		}
 	}
 	void CreateRoot(int data) {
 		root = CreateNode(data);
