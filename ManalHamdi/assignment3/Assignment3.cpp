5fc650c437b65543c68765e80737eaca720e10de
Manal Hamdi <manal.hamdi@yahoo.fr>
2018-06-21 19:13:48 +0200

    Update Assignment3.cpp
    Changes made following your comment

--- ManalHamdi/assignment3/Assignment3.cpp
+++ ManalHamdi/assignment3/Assignment3.cpp
@@ -1,4 +1,3 @@
-//Assumption1: the function should be case insensitive because in real world
 #include <iostream>
 #include <map>
 #include <string>
@@ -17,9 +16,9 @@ class Dictionary {
 		bool isWord(const std::string word) {
 			return words.count(word);
 		}
-		bool isPrefix(const std::string big, const std::string small) {
-			if (big.size() >= small.size()) {
-				return big.substr(0, small.size()) == small;
+		bool isPrefix(const std::string word, const std::string prefix) {
+			if (word.size() >= prefix.size()) {
+				return word.substr(0, prefix.size()) == prefix;
 			}
 			return false;
 		}
@@ -33,15 +32,16 @@ class Dictionary {
 		}
 };
 class Grid {
-public:
-	std::vector<std::vector<char>> grid;
+private: 
 	int number_rows, number_columns;
+	std::vector<std::vector<char>> grid;
+public:
+	Grid(const std::vector<std::vector<char>> grid) {
+		this->grid = grid;
+	}
 };
-bool valid_index(int row, int column, int grid_number_rows, int grid_number_columns) {
-	if (row >= 0 && row < grid_number_rows && column >= 0 && column < grid_number_columns)
-		return true;
-	else
-		return false;
+bool validIndex(int row, int column, int grid_number_rows, int grid_number_columns) {
+	return row >= 0 && row < grid_number_rows && column >= 0 && column < grid_number_columns;
 }
 std::vector<pair<int,int> > getNeighboorIndices(int current_index_row, int current_index_column, int grid_number_rows, int grid_number_columns) {
 	std::vector<std::pair<int,int>> neighboors_indeces;
@@ -55,16 +55,16 @@ std::vector<pair<int,int> > getNeighboorIndices(int current_index_row, int curre
 	return neighboors_indeces;
 }
 void DFS(Dictionary &dictionary,Grid &grid, std::vector<std::string> &words_found, int current_index_row, int current_index_column, std::string &word, std::vector<std::vector<bool> > &visited) {
-	if(visited[current_index_row][current_index_column])
+	if(visited[current_index_row][current_index_column]) {
 		return;
+	}
 	visited[current_index_row][current_index_column] = true;
 	word.push_back(grid[current_index_row][current_index_column]);
-	if (dictionary.isPrefix(word)) {
-		if (dictionary.isWord(word))
-			words_found.push_back(word);
-	}
-	else {
+	if (!dictionary.isPrefix(word)) {
 		return;
+	}
+	if (dictionary.isWord(word)) {
+		words_found.push_back(word);
 	}
 	std::vector<pair<int,int>> neighboors_indeces = getNeighboorIndices(current_index_row, current_index_column, grid.number_rows, grid.number_columns);
 	for (auto neighboor: neighboors_indeces) {
@@ -73,14 +73,23 @@ void DFS(Dictionary &dictionary,Grid &grid, std::vector<std::string> &words_foun
 	visited[current_row][current_column] = false;
 	word.pop_back();
 }
-
+void validateGrid(Grid &grid) {
+	for (int i = 0; i < grid.number_rows; i++) {
+		for (int j = 0; j < grid.number_columns; j++) {
+			grid[i][j] = tolower(grid[i][j]);
+		}
+	}
+}
+//Assumption: the function should be case insensitive because in real world "eat" and "EAT" stand for the same words
 std::vector<std::string> findAllWordsInGrid(Dictionary &dictionary, Grid &grid) {
 	std::vector<std::string> words_grid;
 	std::string word;
 	std::vector<std::vector<bool> > visited(grid.number_rows, vector<bool>(grid.number_columns, false));
-
-	for (int i = 0; i < dictionary.number_rows; i++) {
-		for (int j = 0; j < dictionary.number_columns; j++) {
+	
+	validateGrid(grid);
+	
+	for (int i = 0; i < grid.number_rows; i++) {
+		for (int j = 0; j < grid.number_columns; j++) {
 			DFS(dictionary, grid, words_grid, grid[i][j],i,j, word, visited);
 		}
 	}
