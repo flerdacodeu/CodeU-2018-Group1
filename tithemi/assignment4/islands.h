f7bed8390fa2a782af2611629db8a92cf98a0bcf
Elizaveta Korneeva <elisavetinaaa@gmail.com>
2018-07-11 00:22:41 +0300

    Comments added

--- tithemi/assignment4/islands.h
+++ tithemi/assignment4/islands.h
@@ -3,16 +3,21 @@
 #include <array>
 #include <vector>
 #include <set>
-#include <iostream>
 
 template<size_t Rows, size_t Columns>
 size_t count_islands(const std::array<std::array<bool, Columns>, Rows>& map)
 {
-    size_t counter = -1;
-
-    std::vector<size_t*> islands;
+    // After the following loops each element of the `island_ptr` 
+    // will hold a pointer to a element's corresponding island index.
+    // Elements that corresponds to an `ocean` tile will hold nullptr
     std::array<std::array<size_t*, Columns>, Rows> island_ptr{};
+    // `islands` hold all island-index pointers that will be allocated
+    std::vector<size_t*> islands;
 
+    // The main idea of the algorithm is that for each tile 
+    // we look at it's neighboring tile's and pick current tile's island according to surrounding.
+    // Each iteration of a loops will check only two `tile's` neighbors (a left and a top one),
+    // this way, all neighboring pairs of tiles will be examined.
     for (size_t i = 0; i < Rows; ++i)
     {
         for (size_t j = 0; j < Columns; ++j)
@@ -20,36 +25,43 @@ size_t count_islands(const std::array<std::array<bool, Columns>, Rows>& map)
             if (!map[i][j])
                 continue;
 
+            // Get top and left tile's island-index pointers
+            // and assume that non-existent tiles holds nullptr.
+            // (for first line and first column)
             size_t* top = i ? island_ptr[i - 1][j] : nullptr;
             size_t* left = j ? island_ptr[i][j - 1] : nullptr;
 
-            if (top)
+            if (top) // if a top tile is a land
             {
-                if (left)
+                if (left) // and a left one is a land
                 {
-                    *top = *left;
+                    // ensure that both of them have the exact same island index.
+                    // Note that different pointers can hold the same value.
+                    *top = *left; 
                     island_ptr[i][j] = left;
                 }
-                else
+                else // and a left one is an ocean
                 {
                     island_ptr[i][j] = top;
                 }
             }
-            else
+            else // if a top tile is an ocean
             {
-                if (left)
+                if (left) // and a left one is a land
                 {
                     island_ptr[i][j] = left;
                 }
-                else
+                else // and a left one is an ocean
                 {
-                    islands.push_back(new size_t(++counter));
+                    // The only thing that we can do is to assume that this is a top-left tile of a new unexamined island
+                    islands.push_back(new size_t(islands.size()));
                     island_ptr[i][j] = islands.back();
                 }
             }
         }
     }
 
+    // Now all we have to do is count the number of different island indices and free up resources
     std::set<size_t> unique;
     for (const auto& row : island_ptr)
         for (const auto& ptr : row)
